43f8345270f1501202ea43e2bd937a5340dab7e5
Benoit Maggi <benoit.maggi@gmail.com>
2018-06-20 14:23:14 +0100

    [Quality] Use lamdas :)
     - add unit tests
     - refactor queries:
        - remove unused methods
        - use java 8 api
     - use tycho 1.2.0
    
    Signed-off-by: Benoit Maggi <benoit.maggi@gmail.com>

--- com.github.bmaggi.sysml14.gendoc.queries.tests/src/com/github/bmaggi/sysml14/gendoc/queries/tests/SysML14QueriesTest.java
+++ com.github.bmaggi.sysml14.gendoc.queries.tests/src/com/github/bmaggi/sysml14/gendoc/queries/tests/SysML14QueriesTest.java
@@ -2,6 +2,7 @@ package com.github.bmaggi.sysml14.gendoc.queries.tests;
 
 import static org.junit.Assert.assertEquals;
 
+import org.eclipse.uml2.uml.Class;
 import org.eclipse.uml2.uml.Package;
 import org.eclipse.uml2.uml.UMLFactory;
 import org.junit.Test;
@@ -11,10 +12,32 @@ import com.github.bmaggi.sysml14.gendoc.queries.SysML14Queries;
 public class SysML14QueriesTest {
 
 	@Test
-	public void testDisplayComment() {
-		Package createPackage = UMLFactory.eINSTANCE.createPackage();
-		createPackage.createOwnedComment().setBody("Hello");
-		assertEquals("Hello"+System.getProperty("line.separator"), SysML14Queries.displayComment(createPackage));
+	public void testDisplay0Comment() {
+		Package pkge = UMLFactory.eINSTANCE.createPackage();
+		assertEquals("", SysML14Queries.displayComment(pkge));
 	}
 
+	
+	@Test
+	public void testDisplay1Comment() {
+		Package pkge = UMLFactory.eINSTANCE.createPackage();
+		pkge.createOwnedComment().setBody("Hello");
+		assertEquals("Hello"+System.getProperty("line.separator"), SysML14Queries.displayComment(pkge));
+	}
+
+	@Test
+	public void testDisplay2Comment() {
+		Package pkge = UMLFactory.eINSTANCE.createPackage();
+		pkge.createOwnedComment().setBody("Hello");
+		pkge.createOwnedComment().setBody("Bye!");
+		assertEquals("Hello"+System.getProperty("line.separator")+"Bye!"+System.getProperty("line.separator"), SysML14Queries.displayComment(pkge));
+	}	
+	
+	@Test
+	public void testDisplayOperation() {
+		Class clazz = UMLFactory.eINSTANCE.createClass();
+		clazz.createOwnedOperation("operation", null, null);
+		assertEquals("operation\n", SysML14Queries.displayOperation(clazz));
+	}	
+	
 }
--- com.github.bmaggi.sysml14.gendoc.queries/src/com/github/bmaggi/sysml14/gendoc/queries/SysML14Queries.java
+++ com.github.bmaggi.sysml14.gendoc.queries/src/com/github/bmaggi/sysml14/gendoc/queries/SysML14Queries.java
@@ -2,34 +2,37 @@ package com.github.bmaggi.sysml14.gendoc.queries;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.eclipse.papyrus.sysml14.deprecatedelements.FlowPort;
 import org.eclipse.papyrus.sysml14.portsandflows.FlowDirection;
 import org.eclipse.uml2.uml.Classifier;
 import org.eclipse.uml2.uml.Comment;
 import org.eclipse.uml2.uml.Element;
-import org.eclipse.uml2.uml.Model;
 import org.eclipse.uml2.uml.NamedElement;
-import org.eclipse.uml2.uml.Operation;
 import org.eclipse.uml2.uml.Port;
 import org.eclipse.uml2.uml.Property;
 import org.eclipse.uml2.uml.util.UMLUtil;
 
 public class SysML14Queries {
 
-	// TODO in Java 8...
-	public static String displayComment(NamedElement e) {
-		StringBuilder stringBuilder = new StringBuilder();
-		for (Comment com : e.getOwnedComments()) {
-			stringBuilder.append(com.getBody());
-			stringBuilder.append(System.getProperty("line.separator"));
-		}
-		return stringBuilder.toString();
+	public static String displayComment(NamedElement namedElement) {
+		return namedElement.getOwnedComments().stream()
+				.map(Comment::getBody)
+				.map(body -> body + System.getProperty("line.separator"))
+				.collect(Collectors.joining());
 	}
 
 	public static String displayPart(NamedElement e) {
 		StringBuilder stringBuilder = new StringBuilder();
-		for (NamedElement ne : getParts((Classifier) e)) {
+		LinkedList<NamedElement> parts = new LinkedList<>();
+		for (Property prop : ((Classifier) e).getAllAttributes()) {
+			if (prop.getType() instanceof org.eclipse.uml2.uml.Class) {
+				parts.add((NamedElement) prop);
+			}
+		}
+		
+		for (NamedElement ne : parts) {
 			stringBuilder.append("Part : ");
 			stringBuilder.append(ne.getName());
 			if (((Property) ne).getType() != null) {
@@ -41,6 +44,36 @@ public class SysML14Queries {
 		return stringBuilder.toString();
 	}
 
+	public static String displayAttribute(NamedElement namedElement) {
+		StringBuilder stringBuilder = new StringBuilder();
+		if (namedElement instanceof Classifier) {
+			LinkedList<NamedElement> attributes = new LinkedList<>();
+			for (Property prop : ((Classifier) namedElement).getAllAttributes()) {
+				if (!(prop instanceof Port) && !(prop.getType() instanceof org.eclipse.uml2.uml.Class)) {
+					attributes.add((NamedElement) prop);
+				}
+			}
+			for (NamedElement ne : attributes) {
+				stringBuilder.append(ne.getName());
+				stringBuilder.append("\n");
+			}
+		}
+		return stringBuilder.toString();
+	}
+
+	public static String displayOperation(NamedElement namedElement) {
+		StringBuilder stringBuilder = new StringBuilder();
+		if (namedElement instanceof Classifier) {
+			for (NamedElement ne : ((Classifier) namedElement).getAllOperations()) {
+				stringBuilder.append(ne.getName());
+				stringBuilder.append("\n");
+			}
+
+		}
+		return stringBuilder.toString();
+	}	
+	
+	
 	public static String displayPort(NamedElement e) {
 		StringBuilder stringBuilder = new StringBuilder();
 		for (NamedElement ne : getPort(e)) {
@@ -96,23 +129,7 @@ public class SysML14Queries {
 		return stringBuilder.toString();
 	}
 
-	public static String displayAttribute(NamedElement e) {
-		StringBuilder stringBuilder = new StringBuilder();
-		for (NamedElement ne : getAttributs((Classifier) e)) {
-			stringBuilder.append(ne.getName());
-			stringBuilder.append("\n");
-		}
-		return stringBuilder.toString();
-	}
 
-	public static String displayOperation(NamedElement e) {
-		StringBuilder stringBuilder = new StringBuilder();
-		for (NamedElement ne : getOperations((Classifier) e)) {
-			stringBuilder.append(ne.getName());
-			stringBuilder.append("\n");
-		}
-		return stringBuilder.toString();
-	}
 
 	public static List<NamedElement> getPort(NamedElement c) {
 		LinkedList<NamedElement> ports = new LinkedList<>();
@@ -122,69 +139,6 @@ public class SysML14Queries {
 			}
 		}
 		return ports;
-	}
-
-	public static List<NamedElement> getParts(Classifier c) {
-		LinkedList<NamedElement> parts = new LinkedList<>();
-		for (Property prop : c.getAllAttributes()) {
-			if (prop.getType() instanceof org.eclipse.uml2.uml.Class) {
-				parts.add((NamedElement) prop);
-			}
-		}
-		return parts;
-	}
-
-	public static List<NamedElement> getAttributs(Classifier c) {
-		LinkedList<NamedElement> attributes = new LinkedList<>();
-		for (Property prop : c.getAllAttributes()) {
-			if (!(prop instanceof Port) && !(prop.getType() instanceof org.eclipse.uml2.uml.Class)) {
-				attributes.add((NamedElement) prop);
-			}
-		}
-		return attributes;
-	}
-
-	public static List<NamedElement> getOperations(Classifier c) {
-		LinkedList<NamedElement> operations = new LinkedList<>();
-		for (Operation op : c.getAllOperations()) {
-			operations.add((NamedElement) op);
-		}
-		return operations;
-	}
-
-	public static List<NamedElement> getPackages(Model model) {
-		LinkedList<NamedElement> pakages = new LinkedList<>();
-		for (Element ne : model.getOwnedElements()) {
-			if (ne instanceof org.eclipse.uml2.uml.Package) {
-				pakages.add((NamedElement) ne);
-			}
-		}
-
-		return pakages;
-	}
-
-	public static List<NamedElement> getChilds(Model model) {
-
-		LinkedList<NamedElement> childs = new LinkedList<>();
-		for (Element ne : model.getOwnedElements()) {
-			if ((ne instanceof org.eclipse.uml2.uml.Class || ne instanceof org.eclipse.uml2.uml.Package)
-					&& !contains(childs, (NamedElement) ne)) {
-				childs.add((NamedElement) ne);
-			}
-		}
-
-		return childs;
-	}
-
-	public static boolean contains(List<NamedElement> childs, NamedElement n) {
-		boolean ok = false;
-		for (NamedElement ne : childs) {
-			if (ne.getQualifiedName().equals(n.getQualifiedName())) {
-				ok = true;
-			}
-		}
-
-		return ok;
 	}
 
 }
